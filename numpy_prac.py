8b18e3ffb5a81fd83e8bfffe827f8affcaa8c958
James Hamilton <jlhamilt@caltech.edu>
2016-09-13 11:32:27 -0700

    Mistoke keyword comments for comment in loadtxt

--- numpy_prac.py
+++ numpy_prac.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-xa_high = np.loadtxt('data/xa_high_food.csv', comment='#')
-xa_low = np.loadtxt('data/xa_low_food.csv', comment='#')
+xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
+xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
 
 def xa_to_diam(x):
     """Converts an array of cross-sectional areas to
