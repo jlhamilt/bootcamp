63398b363066d78eb6414ac123ea84b405f1c53c
James Hamilton <jlhamilt@caltech.edu>
2016-09-13 13:36:30 -0700

    Better binning function

--- plotting_round_one.py
+++ plotting_round_one.py
@@ -2,18 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the food data
-xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
-xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
-
 # Set matplotlib rc parameters
 rc = {'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
 sns.set(rc=rc)
 
-# Make the bins
-bins = np.arange(1700, 2500, 50)
+# Load the food data
+xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
+xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
+
+# Make the bin boundaries
+(low_min, low_max) = (np.min(xa_low), np.max(xa_low))
+high_min, high_max = (np.min(xa_high), np.max(xa_high))
+glb_min = np.min([low_min, high_min])
+glb_max = np.max([low_max, high_max])
+bins = np.arange(glb_min-50, glb_max+50, 50)
+
 # Plot the data as a histogram.
-_ = plt.hist(xa_low, bins=bins)
+_ = plt.hist((xa_low, xa_high), bins=bins)
 plt.xlabel("Cross-sectional area ($\mu m ^2$)")
 plt.ylabel('Count')
 plt.show()
