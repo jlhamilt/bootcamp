545331b74d4816f85d0b19ad0102198bc365ad84
James Hamilton <jlhamilt@caltech.edu>
2016-09-13 13:29:34 -0700

    Work with plotting lesson. Starts setting the rc file of seaborn

--- plotting_round_one.py
+++ plotting_round_one.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load the food data
 xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
+
+# Set matplotlib rc parameters
+rc = {'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
+sns.set(rc=rc)
+
+# Make the bins
+bins = np.arange(1700, 2500, 50)
+# Plot the data as a histogram.
+_ = plt.hist(xa_low, bins=bins)
+plt.xlabel("Cross-sectional area ($\mu m ^2$)")
+plt.ylabel('Count')
+plt.show()
